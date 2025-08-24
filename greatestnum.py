2a728f93166c3bb8b70ceca189f05a249b98439b
Kurumurthy@2005 <marennagarikurumurthy2005@gmail.com>
2025-08-24 11:39:58 +0530

    find max Problem 5

---
+++ greatestnum.py
@@ -0,0 +1,24 @@
+# using if else
+num1=10
+num2=20
+
+if num1>num2:
+    print("num1")
+else:
+    print("Num2")
+
+
+
+# using Ternary operator
+
+num1,num2=10,20
+
+result=num1 if num1>num2 else num2
+
+
+
+#Using inbuilt function
+
+
+list=[20,30]
+print(max(list))
