e595424928acb231db9297b645965b8b1d7fe8fa
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-09-01 18:32:37 +0530

    Leap year Problem

--- leapyear.py
+++ leapyear.py
@@ -1,12 +1,41 @@
-x = int(input("Enter Year: "))
+""" x = int(input("Enter Year: "))
 
 if x % 4 == 0:                 
-    if x % 100 == 0:            
-        if x % 400 == 0:        
+    if x % 100 != 0:            
+        if x % 400 != 0:        
             print(x, "is a Leap Year")
         else:
             print(x, "is not a Leap Year")
     else:
         print(x, "is a Leap Year")
 else:
-    print(x, "is not a Leap Year")
+    print(x, "is not a Leap Year") """
+
+
+#Using Ternary operator
+
+""" x=2024
+
+result=True if x%4==0 and x%100!=0 or x%400!=0 else False
+print(result)
+ """
+# USing calender module
+
+""" import calendar
+
+year=2024
+result=calendar.isleap(year)
+print(result) """
+
+
+
+
+leap=lambda year : year%4==0 and year%100!=0 or year%400!=0
+result=leap(2024)
+print(result)
+
+
+
+
+
+
