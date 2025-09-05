d064dc757569991732ccc2d1300eff35f3c206cf
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-09-05 13:50:32 +0530

    Primes in a ranges with continue keyword

--- primes_in_range.py
+++ primes_in_range.py
@@ -1,4 +1,4 @@
-import math
+""" import math
 min=2
 max=25
 list=[]
@@ -18,3 +18,31 @@ for i in range(min,max+1):
 
 
 print(list)
+
+ """
+
+
+import math
+primes=[2,3]
+min=2
+max=10
+
+for i in range(min,max+1):
+    flag=0
+    if i%2==0:
+        continue
+    if i%3==0:
+        continue
+    iter=3
+    while iter<round(math.sqrt(max)):
+        if i%iter==0:
+            flag=1
+            break
+        iter+=2
+
+    if flag==0:
+       primes.append(i)
+
+
+print(primes)
+
