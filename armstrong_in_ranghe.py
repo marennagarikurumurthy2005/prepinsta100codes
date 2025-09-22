2d873ef11c465caf0daa48df50fd41b36259a2d4
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-09-22 22:26:38 +0530

    armstrong list correction

--- armstrong_in_ranghe.py
+++ armstrong_in_ranghe.py
@@ -1,13 +1,15 @@
 minimum=1
-maximum=1800
+maximum=100000
 arm_list=[]
 for i in range(minimum,maximum):
     temp=i
+    num=i
     sum=0
-    for j in range(len(str(i))):
-        reminder=i%10
-        sum=sum+(reminder**3)
-        i=i//10
+    n=len(str(i))
+    while num>0:
+        reminder=num%10
+        sum=sum+(reminder**n)
+        num=num//10
 
     if temp==sum:
         arm_list.append(sum)
