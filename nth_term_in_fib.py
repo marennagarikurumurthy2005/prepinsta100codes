56edfc089430e6ad6830ed945d7c28b9624d2d7c
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-10-09 17:34:05 +0530

    nth fib using for loop

---
+++ nth_term_in_fib.py
@@ -0,0 +1,19 @@
+
+ # FIB using loop   
+""" 
+a, b = 0, 1
+n = int(input("Number: "))
+
+if n == 0:
+    print(a)
+elif n == 1:
+    print(b)
+else:
+    for i in range(2, n + 1):
+        c = a + b
+        a = b
+        b = c
+    print(c)
+ """
+
+
