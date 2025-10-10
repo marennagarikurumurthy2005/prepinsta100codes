1f93e4f51986481f2a03e233b6d5d27f0c619b8b
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-10-10 19:00:21 +0530

    power of a number using for loop and math.pow() function

---
+++ power_of_a _number.py
@@ -0,0 +1,16 @@
+""" # using simple iteration
+num=10
+power=3
+total=1
+if num==0:
+    print(total)
+else:
+    for i in range(0,power):
+        total=total*num
+
+    print(total) """
+
+# using power function
+
+import math
+print(math.pow(3,3))
