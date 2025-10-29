23142d3ae06a402082311d4145b3df8acee6fb79
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-10-29 18:45:49 +0530

    perfect square

---
+++ perfect_square.py
@@ -0,0 +1,15 @@
+import math
+
+
+num=16
+# if math.floor(math.sqrt(num))==math.ceil(math.sqrt(num)):
+#     print("Perfect square")
+# else:
+#     print("Not")
+
+
+
+# product=math.floor(math.sqrt(num))*math.floor(math.sqrt(num))
+# if num==product:
+#     print("Perfect Square")
+
